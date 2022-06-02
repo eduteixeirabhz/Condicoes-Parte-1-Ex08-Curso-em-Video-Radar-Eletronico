81821e9ef7089fbaba21145c6d9171af64a7be4c
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 16:09:38 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel} KM/h`)
+if (vel > 60){ //Condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
